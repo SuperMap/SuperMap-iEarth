b84ab3927265a76bda842a05ae21b4b6f860c547
fishelren <ren_feixiang@126.com>
2018-10-12 13:50:17 +0800

    修改路线和站点上层绘制为底层直接提供 及 未选择fpf文件点击开始飞行报错

--- js/3DGIS/flyRoute.js
+++ js/3DGIS/flyRoute.js
@@ -1,106 +1,83 @@
-define(['Cesium','../lib/SuperMap','../lib/Convert'],function(Cesium, Super, Convert) {
+define(['Cesium', '../lib/SuperMap', '../lib/Convert'], function (Cesium, Super, Convert) {
     'use strict';
     var $ = require('jquery');
 
     /*
-    * 飞行管理对象
-    * 飞行路线加载
-    * 飞行过程管理，开始，暂停，结束
-    * 飞行点管理
-    *
-    * */
+     * 飞行管理对象
+     * 飞行路线加载
+     * 飞行过程管理，开始，暂停，结束
+     * 飞行点管理
+     *
+     * */
     var flyManager;
 
     var flyRoute = function () {
     };
 
-    flyRoute.remove = function(viewer){
-
+    flyRoute.remove = function (viewer) {
     };
 
-    flyRoute.initializing = function(viewer){
-       if(flyManager){
-           flyManager.play();
-       } else {
-           var scene = viewer.scene;
-           var routes = new Cesium.RouteCollection();
-           var fileInput = document.getElementById("flyFile");
-           var file = fileInput.files[0];
-           var reader = new FileReader();
-           reader.onload=function(e) {
-               var XMLContent = e.target.result;
-               routes.fromXML(XMLContent);
-           };
-           reader.readAsBinaryString(file)
-           //创建飞行管理对象
-           flyManager = new Cesium.FlyManager({
-               scene : scene,
-               routes : routes
-           });
-           flyManager.stopArrived.addEventListener(function(routeStop){
-               var stopName = routeStop.stopName;
-               var entity = new Cesium.Entity({
-                   description : '到达站点 : ' + stopName,
-                   name : stopName
-               });
-               viewer.selectedEntity = entity;
-               setTimeout(function(){
-                   viewer.selectedEntity = undefined;
-               },1000);
-               routeStop.waitTime = 1;
-           });
-           if(flyManager.readyPromise){
-               Cesium.when(flyManager.readyPromise,function(){
-                   var allStops = flyManager.getAllRouteStops();
-                   var menu = document.getElementById('stopList');
-                   var flyLine =[];
-                   for(var i = 0,j = allStops.length;i < j;i++){
-                       var option = document.createElement('option');
-                       option.textContent = "站点 "+(i+1);
-                       option.value = allStops[i].index;
-                       menu.appendChild(option);
-                       flyLine.push(allStops[i].point);
-                       viewer.entities.add({
-                           position : allStops[i].point,
-                           billboard :{
-                               image : './images/station.png',
-                               width:30,
-                               height:40,
-                           }
-                       });
-                   }
-                 viewer.entities.add({
-                       polyline : {
-                           positions : flyLine,
-                           width : 10,
-                           followSurface : false,
-                           material : new Cesium.PolylineArrowMaterialProperty(Cesium.Color.RED)
-                       }
-                   });
-                   flyManager.play();
+    flyRoute.initializing = function (viewer) {
+        if (flyManager) {
+            flyManager.play();
+        } else {
+            var scene = viewer.scene;
+            var routes = new Cesium.RouteCollection(viewer.entities);
+            var fileInput = document.getElementById("flyFile");
+            var file = fileInput.files[0];
+            if(!file){
+                return; // 没有选择fpf文件无法开始执行
+            }
+            var reader = new FileReader();
+            reader.onload = function (e) { // 读取操作完成时出发
+                var XMLContent = e.target.result;
+                routes.fromXML(XMLContent);
+            };
+            reader.readAsBinaryString(file);
+            //创建飞行管理对象
+            flyManager = new Cesium.FlyManager({
+                scene: scene,
+                routes: routes
+            });
+            flyManager.stopArrived.addEventListener(function (routeStop) {
+                routeStop.waitTime = 1;
+            });
+            if (flyManager.readyPromise) {
+                Cesium.when(flyManager.readyPromise, function () {
+                    var currentRoute = flyManager.currentRoute;
+                    currentRoute.isLineVisible = true;
+                    currentRoute.isStopVisible = true;
 
-               });
-          }
+                    var allStops = flyManager.getAllRouteStops();
+                    var menu = document.getElementById('stopList');
+                    for (var i = 0, j = allStops.length; i < j; i++) {
+                        var option = document.createElement('option');
+                        option.textContent = "站点 " + (i + 1);
+                        option.value = allStops[i].index;
+                        menu.appendChild(option);
+                    }
+                    //各个站点事件
+                    $('#stopList').change(function () {
+                        flyManager && flyManager.pause();
+                        var index = parseInt($(this).val());
+                        var route = flyManager.currentRoute;
+                        var stop = route.get(index);
+                        flyManager.currentStopIndex = index;
+                        flyManager.viewToStop(stop);
+                    });
+                    flyManager.play();
+                });
+            }
         }
-        //各个站点事件
-        $('#stopList').change(function(){
-            flyManager && flyManager.pause();
-            var index = parseInt($(this).val());
-            var route = flyManager.currentRoute;
-            var stop = route.get(index);
-            flyManager.currentStopIndex = index;
-            flyManager.viewToStop(stop);
-            flyManager && flyManager.pause();
-        });
-    }
+    };
 
     flyRoute.pause = function () {
         flyManager && flyManager.pause();
-    }
+    };
 
     flyRoute.stop = function () {
         flyManager && flyManager.stop();
-    }
+    };
 
     return flyRoute;
 });
