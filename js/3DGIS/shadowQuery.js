3ab901aca1005cea8e04aac1076df686663812f3
heqianhq <heqian@supermap.com>
2019-09-05 11:53:27 +0800

    修改通视分析和阴影分析

--- js/3DGIS/shadowQuery.js
+++ js/3DGIS/shadowQuery.js
@@ -1,4 +1,4 @@
-define(['Cesium'],function(Cesium) {
+define(['Cesium'], function (Cesium) {
     'use strict';
     var shadow = function () {
 
@@ -6,47 +6,49 @@ define(['Cesium'],function(Cesium) {
     var shadowQuery;
     var handlerPolygon;
     var positions;
-    shadow.initializing = function(viewer,sceneModel){
+    shadow.initializing = function (viewer, sceneModel) {
+        if (viewer.shadows == false) {
+            viewer.shadows = true;//开启场景阴影
+        }
         var scene = viewer.scene;
         var layers = scene.layers.layerQueue;
 
-        for(var i = 0;i < layers.length;i++){
+        for (var i = 0; i < layers.length; i++) {
             layers[i].shadowType = 2;
             layers[i].refresh();
         }
         //创建阴影查询对象
-        if(!shadowQuery){
+        if (!shadowQuery) {
             shadowQuery = new Cesium.ShadowQueryPoints(scene);
             shadowQuery.build();
         }
 
         setCurrentTime();
-        handlerPolygon = new Cesium.DrawHandler(viewer,Cesium.DrawMode.Polygon,0);
-        handlerPolygon.activeEvt.addEventListener(function(isActive){
-            if(isActive == true){
+        handlerPolygon = new Cesium.DrawHandler(viewer, Cesium.DrawMode.Polygon, 0);
+        handlerPolygon.activeEvt.addEventListener(function (isActive) {
+            if (isActive == true) {
                 viewer.enableCursorStyle = false;
                 viewer._element.style.cursor = '';
                 $('body').removeClass('drawCur').addClass('drawCur');
-            }
-            else{
+            } else {
                 viewer.enableCursorStyle = true;
                 $('body').removeClass('drawCur');
             }
         });
         var points = [];
 
-        handlerPolygon.drawEvt.addEventListener(function(result){
-            points.length = 0 ;
+        handlerPolygon.drawEvt.addEventListener(function (result) {
+            points.length = 0;
             var polygon = result.object;
-            if(!polygon){
-                return ;
+            if (!polygon) {
+                return;
             }
             polygon.show = false;
             handlerPolygon.polyline.show = false;
             positions = [].concat(polygon.positions);
-            positions = Cesium.arrayRemoveDuplicates(positions,Cesium.Cartesian3.equalsEpsilon);
+            positions = Cesium.arrayRemoveDuplicates(positions, Cesium.Cartesian3.equalsEpsilon);
             //遍历多边形，取出所有点
-            for(var i = 0, len = positions.length; i < len; i++) {
+            for (var i = 0, len = positions.length; i < len; i++) {
                 //转化为经纬度，并加入至临时数组
                 var cartographic = Cesium.Cartographic.fromCartesian(polygon.positions[i]);
                 var longitude = Cesium.Math.toDegrees(cartographic.longitude);
@@ -74,34 +76,34 @@ define(['Cesium'],function(Cesium) {
             var bh = Number($('#bottomHeight').val());
             var eh = Number($('#extrudeHeight').val());
             shadowQuery.qureyRegion({
-                position : points,
-                bottom : bh,
-                extend : eh
+                position: points,
+                bottom: bh,
+                extend: eh
             });
             shadowQuery.build();
         });
-            handlerPolygon.deactivate();
-            handlerPolygon.activate();
+        handlerPolygon.deactivate();
+        handlerPolygon.activate();
 
-            var store = {};
-            store.startTime = shadowQuery.startTime;
-            store.endTime = shadowQuery.endTime;
-            store.position = positions;
-            store.bottom = Number($('#bottomHeight').val());
-            store.extend = Number($('#extrudeHeight').val());
-            sceneModel.analysisObjects.shadowQueryStore = store;
+        var store = {};
+        store.startTime = shadowQuery.startTime;
+        store.endTime = shadowQuery.endTime;
+        store.position = positions;
+        store.bottom = Number($('#bottomHeight').val());
+        store.extend = Number($('#extrudeHeight').val());
+        sceneModel.analysisObjects.shadowQueryStore = store;
 
 
-        $('#sunlight').click(function(){
+        $('#sunlight').click(function () {
             var dateVal = $("#selDate").val();
             var startTime = new Date(dateVal);
             var endTime = new Date(dateVal);
             var shour = Number($("#startTime :selected").val());
             var ehour = Number($("#endTime :selected").val());
-            for(var i = 0;i < layers.length;i++){
+            for (var i = 0; i < layers.length; i++) {
                 layers[i].shadowType = 2;
             }
-            if(shour > ehour) {
+            if (shour > ehour) {
                 return;
             }
 
@@ -112,17 +114,17 @@ define(['Cesium'],function(Cesium) {
             // });
 
             var nTimer = 0.0;
-            var nIntervId = setInterval(function() {
-                if(shour < ehour) {
+            var nIntervId = setInterval(function () {
+                if (shour < ehour) {
                     startTime.setHours(shour);
                     startTime.setMinutes(nTimer);
                     viewer.clock.currentTime = Cesium.JulianDate.fromDate(startTime);
                     nTimer += 10.0;
-                    if(nTimer > 60.0){
+                    if (nTimer > 60.0) {
                         shour += 1.0;
                         nTimer = 0.0;
                     }
-                }else {
+                } else {
                     clearInterval(nIntervId);
                 }
             }, 20);
@@ -130,56 +132,56 @@ define(['Cesium'],function(Cesium) {
         });
 
 
-        $('#clear').click(function(){
+        $('#clear').click(function () {
             handlerPolygon.deactivate();
             // handlerPolygon.polygon.show = false;
             // handlerPolygon.polyline.show = false;
             viewer.entities.removeAll();
-            for(var i = 0;i < layers.length;i++){
+            for (var i = 0; i < layers.length; i++) {
                 layers[i].shadowType = 0;
             }
             shadowQuery.qureyRegion({
-                position : [0,0],
-                bottom : 0,
-                extend : 0
+                position: [0, 0],
+                bottom: 0,
+                extend: 0
             });
         });
 
-        $('#startTime').change(function(){
+        $('#startTime').change(function () {
             var startTime = new Date($("#selDate").val());
             startTime.setHours(Number($(this).val()));
             shadowQuery.startTime = Cesium.JulianDate.fromDate(startTime);
         });
-        $('#endTime').change(function(){
+        $('#endTime').change(function () {
             var endTime = new Date($("#selDate").val());
             endTime.setHours(Number($(this).val()));
             shadowQuery.endTime = Cesium.JulianDate.fromDate(endTime);
             setCurrentTime();
         });
 
-        $('#bottomHeight').on("input propertychange", function(){
-            if(this.value === ""){
+        $('#bottomHeight').on("input propertychange", function () {
+            if (this.value === "") {
                 $(this).val("0.0");
             }
             var bh = Number($(this).val());
             var eh = Number($('#extrudeHeight').val());
             shadowQuery.qureyRegion({
-                position : points,
-                bottom : bh,
-                extend : eh
+                position: points,
+                bottom: bh,
+                extend: eh
             });
         });
 
-        $('#extrudeHeight').on("input propertychange", function(){
-            if(this.value === ""){
+        $('#extrudeHeight').on("input propertychange", function () {
+            if (this.value === "") {
                 $(this).val("0.0");
             }
             var bh = Number($('#bottomHeight').val());
             var eh = Number($(this).val());
             shadowQuery.qureyRegion({
-                position : points,
-                bottom : bh,
-                extend : eh
+                position: points,
+                bottom: bh,
+                extend: eh
             });
         });
 
@@ -191,22 +193,21 @@ define(['Cesium'],function(Cesium) {
             viewer.clock.shouldAnimate = true;
         }
     }
-    shadow.remove = function(viewer){
-        if(!handlerPolygon){
+    shadow.remove = function (viewer) {
+        if (!handlerPolygon) {
             return;
-        }
-        else{
+        } else {
             handlerPolygon.deactivate();
         }
         var layers = viewer.scene.layers.layerQueue;
-        for(var i = 0;i < layers.length;i++){
+        for (var i = 0; i < layers.length; i++) {
             layers[i].shadowType = 0;
         }
-        if(shadowQuery){
+        if (shadowQuery) {
             shadowQuery.qureyRegion({
-                position : [0,0],
-                bottom : 0,
-                extend : 0
+                position: [0, 0],
+                bottom: 0,
+                extend: 0
             });
             shadowQuery.destroy();
             shadowQuery = undefined;
