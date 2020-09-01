ecf8e8453801b60b411e018809a20fe9466372d9
chenfei23 <chenfei1@supermap.com>
2020-09-01 17:30:17 +0800

    1、修改特效
    Signed-off-by: chenfei23 <chenfei1@supermap.com>

--- data/examples/js/config.js
+++ data/examples/js/config.js
@@ -1,5 +1,6 @@
 var URL_CONFIG = {
-    SCP_WORLD_COUNTRY_VECTOR: 'http://www.supermapol.com/realspace/services/3D-Countries-World2/rest/realspace/datas/Countries_1@World/config', // 全球国家边界线的矢量
+    SCP_WORLD_COUNTRY_VECTOR2: 'http://www.supermapol.com/realspace/services/3D-ShiJieGuoJiaBianJie/rest/realspace', // 全球国家边界线的矢量
     SCENE_POLYLINEGROW_BUINDINGS: 'http://www.supermapol.com/realspace/services/3D-buildings1122/rest/realspace', // 光晕效果线数据
-    SCENE_CBD: 'http://www.supermapol.com/realspace/services/3D-CBD/rest/realspace' // CBD场景
+    SCENE_CBD: 'http://www.supermapol.com/realspace/services/3D-CBDCache20200416/rest/realspace' // CBD场景
 };
+
