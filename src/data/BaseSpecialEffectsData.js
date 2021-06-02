a8f8e8a7ba9c14d9fc3e70f9642598da7189eafc
Peter <bjfubjfu@gmail.com>
2021-06-02 09:52:41 +0800

    replaced with vue-earth by gaozhx

---
+++ src/data/BaseSpecialEffectsData.js
@@ -0,0 +1,21 @@
+var BaseSpecialEffectModels;
+export default BaseSpecialEffectModels = [
+  {
+    "name":"云层",
+    "proxiedUrl":"@/../static/images/clouds-supermap-sm.png",
+    "description": "云层"
+  },
+  {
+    "name": "天空盒",
+    "id": 5,
+    "skyBoxRight": "@/../static/images/SkyBox/Right.png",
+    "skyBoxLeft":"@/../static/images/SkyBox/Left.png",
+    "skyBoxFront": "@/../static/images/SkyBox/Front.png",
+    "skyBoxBack": "@/../static/images/SkyBox/Back.png",
+    "skyBoxUp": "@/../static/images/SkyBox/Up.png",
+    "skyBoxDown": "@/../static/images/SkyBox/Down.png",
+    "description": "天空盒",
+    "currentSky":null,
+    "defaultSky":null
+  }
+];
