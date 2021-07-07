07de9ae4b06fb9f8ea54ef8305551b4f7355be06
Peter <bjfubjfu@gmail.com>
2021-07-07 17:36:18 +0800

    update the latest version by gaozhx

--- data/examples/js/config.js
+++
@@ -1,6 +0,0 @@
-var URL_CONFIG = {
-    SCP_WORLD_COUNTRY_VECTOR2: 'http://www.supermapol.com/realspace/services/3D-ShiJieGuoJiaBianJie/rest/realspace', // 全球国家边界线的矢量
-    SCENE_POLYLINEGROW_BUINDINGS: 'http://www.supermapol.com/realspace/services/3D-buildings1122/rest/realspace', // 光晕效果线数据
-    SCENE_CBD: 'http://www.supermapol.com/realspace/services/3D-CBDCache20200416/rest/realspace' // CBD场景
-};
-
--- js/views/ViewerContainer.js
+++
@@ -1,9 +0,0 @@
-define(['./Container'],function(Container){
-    var ViewerContainer = Container.extend({
-        el : '#cesiumContainer',
-        render : function() {
-            return this;
-        }
-    });
-    return ViewerContainer;
-});
