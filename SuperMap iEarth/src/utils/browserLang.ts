193e5180a854ed6d39d78239e3dd080c7bc8aad2
supermap123 <huangyiqin@supermap>
2024-10-28 10:51:06 +0800

    国际化支持优先cooike再浏览器本身语言

--- SuperMap iEarth/src/utils/browserLang.ts
+++ SuperMap iEarth/src/utils/browserLang.ts
@@ -5,34 +5,38 @@ export const getBrowserLanguageSub = () => {
     //浏览器语言: chrome+360用language; Edge用userLanguage; IE用browserLanguage。
     let navigator: any = window.navigator;
     let language = navigator.language || navigator.userLanguage || navigator.browserLanguage;
-    let lang = language.toLowerCase();
+    let browserLang = language.toLowerCase();
+
+    // 通过iportal设置语言，从cookie获取
+    let cookieLang:any = undefined;
+    const cookies = document.cookie.split(';');
+    if (cookies.length > 0) {
+        cookies.forEach(function (cookie) {
+            const arr = cookie.split('=');
+            if (arr[0].toLowerCase().trim() === 'language') {
+                cookieLang = arr[1];
+            }
+        });
+    }
+
+    // 将浏览器语言字符串统一，返回两位：['zh', 'en', 'ja', 'ru'];
+    function computedLang(langstr):string{
+        let result:string = '';
+        if (langstr.indexOf('-') != -1) {
+            result = langstr.split('-')[0];
+        } else if (langstr.length > 2) {
+            result = langstr.substr(0, 2);
+        }else if(langstr.length == 2){
+            result = langstr;
+        }
+        return result;
+    }
 
     let result: string = '';
-
-    if (lang.indexOf('-') != -1) {
-        result = lang.split('-')[0];
-    } else if (lang.length > 2) {
-        result = lang.substr(0, 2);
-    }else if(lang.length == 2){
-        result = lang;
+    if(cookieLang){ 
+        result = computedLang(cookieLang); // 优先使用cookie（iPortal）
+    }else if(browserLang){
+        result = computedLang(browserLang); // 没有cookie再自动识别浏览器语言
     }
     return result;
-
-    // 暂时不考虑iportal cookies
-    // //判断portal设置语言 通过cookie获取
-    // const cookies = document.cookie.split(';');
-    // if (cookies.length > 0) {
-    //     cookies.forEach(function (cookie) {
-    //         const arr = cookie.split('=');
-    //         if (arr[0].toLowerCase().trim() === 'language') {
-    //             result = arr[1];
-    //             return;
-    //         }
-    //     });
-    // }
-
-    // // 如果从cookies中获取语言了，就直接返回
-    // if(result != '' && result.length == 2){
-    //     return result;
-    // }
 };
