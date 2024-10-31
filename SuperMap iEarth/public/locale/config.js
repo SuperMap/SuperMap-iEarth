0298da660b5b980d06037a22891006f7762f008b
supermap123 <huangyiqin@supermap>
2024-10-31 14:33:41 +0800

    主版本1130国际化支持法语和西班牙语

--- SuperMap iEarth/public/locale/config.js
+++ SuperMap iEarth/public/locale/config.js
@@ -1,10 +1,12 @@
-const lang_support_list = ["zh", "en", "ja", "ru"]; // 支持浏览器自动识别的语言列表
+const lang_support_list = ["zh", "en", "ja", "ru", "es", "fr"]; // 支持浏览器自动识别的语言列表
 
 const lang_data = {  // vue-i18n：createI18n中的messages
     zh:local_zh,
     en:local_en,
     ja:local_ja,
     ru:local_ru,
+    es:local_es,
+    fr:local_fr,
 }
 
 const lang_default = 'en'; // 当前浏览器语言不在支持列表时的默认语言
