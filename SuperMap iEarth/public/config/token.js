f7724fb6cc1d5c2aab55eb8f11b4e8c83d7f6da8
huangyiqin <58579283+supermap123@users.noreply.github.com>
2025-06-06 10:17:06 +0800

    更新必应地图key

--- SuperMap iEarth/public/config/token.js
+++ SuperMap iEarth/public/config/token.js
@@ -1,6 +1,6 @@
 const configToken = {
     "tiandituKey":"97d2d09de3e54939dd20123859260bf9", 
-    "bingMapsKey":"AiCyZH6DplpqBK5CViec7lYcLq941OtnIAvlcVojsyxBfZwUDvp0CsHzZr--U2KY"
+    "bingMapsKey":"AkNh5OpOWgi_CnmMMlh1pCEv7OGqHKnsK4XU1mxPbUSVq21ncmnZm4kf-lBh2hxw"
 }
 
 window.configToken = configToken;
